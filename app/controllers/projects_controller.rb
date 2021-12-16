1944ce2ff81c84d8d013ab0eea2b9c6136f191b4
Nair Designs` <nairbdg@gmail.com>
2021-12-16 22:35:48 +0900

    pages finally working!! projects and people

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,9 @@
 class ProjectsController < ApplicationController
+<<<<<<< HEAD
   before_action :set_project, only: [:show, :edit,:update, :destroy]
 end
+=======
+  def index
+  end
+end
+>>>>>>> 1dacbf3 (pages finally working!! projects and people)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
 
+<<<<<<< HEAD
   resources :projects
 
+=======
+  get 'people/index'
+  get 'projects/index'
+  resources :projects
+  resources :people
+>>>>>>> 1dacbf3 (pages finally working!! projects and people)
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
